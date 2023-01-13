e5e3f4a2dde9547f5a7dd213146a9523489fdd46
favourite1975 <godsfavour1975@gmail.com>
2023-01-13 23:44:31 +0100

    fixed

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -8,11 +8,11 @@ const HomePage = () => {
                 <Navbar />
             </header>
 
-            <main className='w-full flex justify-between items-center px-10 py-10 '>
+            <main className='w-full flex justify-between items-center px-10 py-20 '>
 
-                <div className='w-1/2 px-4'>
+                <div className='w-1/2 px-4 pt-10'>
                    <div className='font-bold text-3xl '>Hi! am <span className='text-primary'>Elimihele</span> God’s Favour</div> 
-                   <p className='w-[72%] py-5 leading-[30px]'>
+                   <p className='w-[72%] py-5 leading-[30px] text-[16px]'>
                     Am a skilled, experienced and Solution-oriented web-developer,
                     and an open-source contributor experienced in building and maintaining
                      software and software architecture
@@ -22,7 +22,7 @@ const HomePage = () => {
                    </div>
                 </div>
 
-                <div className='w-1/2 pl-40 pt-10 flex items-center justify-center'>
+                <div className='w-1/2 pl-40 flex items-center justify-center'>
                     <div className='flex items-center justify-center'>
                         <Image
                         src={PhantomImg}
