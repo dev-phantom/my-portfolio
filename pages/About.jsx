ef6644e44a9ff6d14e6b15a40794e6faab2cefae
favourite1975 <godsfavour1975@gmail.com>
2023-01-29 18:52:57 +0100

    fix: fixed the padding issue on the home page

--- pages/About.jsx
+++ pages/About.jsx
@@ -7,18 +7,18 @@ const About = () => {
             <div>
                 <Navbar />
             </div>
-            <h1 className="font-semibold text-4xl py-10 text-center">About-<span className="text-primary">me</span></h1>
+            <h1 className="font-semibold text-3xl md:text-4xl py-10 text-center">About-<span className="text-primary">me</span></h1>
             <div className="w-full flex justify-center items-center ">
-                <p className=" bg-altBg w-[80%] font-semibold text-xl p-12 rounded-lg leading-[40px]">
+                <p className=" bg-altBg w-11/12 md:w-[80%] font-semibold text-md  md:text-xl p-5 md:p-12 rounded-lg leading-[25px] md:leading-[40px]">
                     I am a <span className="text-secondary">diligent</span> web developer/designer & an open source contributor with 5 years of experience in building user-friendly web-applications & web-sites. 
                     Highly skilled in communication, <span className="text-secondary">collaboration,</span> and designing of appealing user-centered, and friendly interfaces, Building and designing responsive web components, with modern technologies such as HTML, CSS & Javascript including frameworks/library like, <span className="text-secondary">ReactJs,</span> NextJs, Typescript, JQuery and css libraries such as TailwindCss, ChakraUi, bootstrap, SCSS e.t.c.
                     I also use <span className="text-secondary">Firebase</span> & Supabase as a BaaS technology and <span className="text-secondary">Github</span> & Bitbucket
                     as a cloud storage technology 
                 </p>
             </div>
-            <div className="px-40">
+            <div className=" px-10 md:px-40">
                 <h1 className="font-semibold text-2xl py-10 ">My <span className="text-primary">Experiences</span></h1>
-                <div className="grid grid-cols-2 w-full gap-10">
+                <div className="grid grid-col-1 md:grid-cols-2 w-full gap-10">
                     <div className="bg-altBg rounded-lg  p-4 border-l-2 border-orange">
                        <h2 className="text-xl font-bold py-1 capitalize ">Praisetech <span className="text-secondary">Enterprise</span></h2>
                        <h4 className="font-medium text-md ">senior web developer</h4> 
